b03add48bce6d466bb2303748a23f8033d5e1e7a
Xeon0X <xeon0x.gaming@gmail.com>
2025-10-24 07:43:26 +0200

    Remove networks tools

--- configuration.nix
+++ configuration.nix
@@ -194,24 +194,8 @@
     vulkan-tools
     # sbctl # For Lanzaboote
     chromium
-    delta
     esptool
-    inetutils
-    dig
   ];
-
-  services.postfix = {
-    enable = true;
-    config = {
-      inet_interfaces = "localhost";
-      mydomain = "onyx.ovh";
-    };
-  };
-
-  services.dovecot2 = {
-    enable = true;
-    enablePop3 = true;
-  };
 
   virtualisation.docker = {
     enable = true;
@@ -271,10 +255,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [
-    24800
-    110
-  ];
+  networking.firewall.allowedTCPPorts = [ 24800 ];
   networking.firewall.allowedUDPPorts = [ 24800 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
