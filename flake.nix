2f4886655a191186dfae7a598749eca0cbccdf97
Xeon0X <xeon0x.gaming@gmail.com>
2026-01-29 13:43:35 +0100

    fix: use patched nixos-hardware

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:8bitbuddhist/nixos-hardware/surface-kernel-6.18";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs"; # Follows stable nixpkgs by default
